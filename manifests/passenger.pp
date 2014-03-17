f0f03d75245e72746868e81db60abadf11062177
Guillaume Espanel <guillaume.espanel@objectif-libre.com>
2014-03-17 14:38:27 +0100

    Parameterize the Apache SSL settings

--- manifests/passenger.pp
+++ manifests/passenger.pp
@@ -37,7 +37,13 @@ class puppet::passenger(
   $puppet_conf,
   $puppet_ssldir,
   $certname,
-  $conf_dir
+  $conf_dir,
+  $ssl_cert  = "${puppet_ssldir}/certs/${certname}.pem",
+  $ssl_key   = "${puppet_ssldir}/private_keys/${certname}.pem",
+  $ssl_chain = "${puppet_ssldir}/ca/ca_crt.pem",
+  $ssl_ca    = "${puppet_ssldir}/ca/ca_crt.pem",
+  $ssl_crl   = "${puppet_ssldir}/ca/ca_crl.pem",
+  $ca = true,
 ){
   include apache
   include puppet::params
@@ -52,42 +58,46 @@ class puppet::passenger(
       mode   => '0750',
     }
 
-    file{"${puppet_ssldir}/ca":
-      ensure => directory,
-      owner  => $::puppet::params::puppet_user,
-      group  => $::puppet::params::puppet_group,
-      mode   => '0770',
-      before => Exec['Certificate_Check'],
-    }
-
-    file{"${puppet_ssldir}/ca/requests":
-      ensure => directory,
-      owner  => $::puppet::params::puppet_user,
-      group  => $::puppet::params::puppet_group,
-      mode   => '0750',
-      before => Exec['Certificate_Check'],
+    if($ca == true) {
+      file{"${puppet_ssldir}/ca":
+        ensure => directory,
+        owner  => $::puppet::params::puppet_user,
+        group  => $::puppet::params::puppet_group,
+        mode   => '0770',
+        before => Exec['Certificate_Check'],
+      }
+  
+      file{"${puppet_ssldir}/ca/requests":
+        ensure => directory,
+        owner  => $::puppet::params::puppet_user,
+        group  => $::puppet::params::puppet_group,
+        mode   => '0750',
+        before => Exec['Certificate_Check'],
+      }
     }
   }
 
-  # first we need to generate the cert
-  # Clean the installed certs out ifrst
-  $crt_clean_cmd  = "puppet cert clean ${certname}"
-  # I would have preferred to use puppet cert generate, but it does not
-  # return the corret exit code on some versions of puppet
-  $crt_gen_cmd   = "puppet certificate --ca-location=local --dns_alt_names=puppet generate ${certname}"
-  # I am using the sign command here b/c AFAICT, the sign command for certificate
-  # does not work
-  $crt_sign_cmd  = "puppet cert sign --allow-dns-alt-names ${certname}"
-  # find is required to move the cert into the certs directory which is
-  # where it needs to be for puppetdb to find it
-  $cert_find_cmd = "puppet certificate --ca-location=local find ${certname}"
+  if($ca == true) {
+    # first we need to generate the cert
+    # Clean the installed certs out ifrst
+    $crt_clean_cmd  = "puppet cert clean ${certname}"
+    # I would have preferred to use puppet cert generate, but it does not
+    # return the corret exit code on some versions of puppet
+    $crt_gen_cmd   = "puppet certificate --ca-location=local --dns_alt_names=puppet generate ${certname}"
+    # I am using the sign command here b/c AFAICT, the sign command for certificate
+    # does not work
+    $crt_sign_cmd  = "puppet cert sign --allow-dns-alt-names ${certname}"
+    # find is required to move the cert into the certs directory which is
+    # where it needs to be for puppetdb to find it
+    $cert_find_cmd = "puppet certificate --ca-location=local find ${certname}"
 
-  exec { 'Certificate_Check':
-    command   => "${crt_clean_cmd} ; ${crt_gen_cmd} && ${crt_sign_cmd} && ${cert_find_cmd}",
-    unless    => "/bin/ls ${puppet_ssldir}/certs/${certname}.pem",
-    path      => '/usr/bin:/usr/local/bin',
-    logoutput => on_failure,
-    require   => File[$puppet_conf]
+    exec { 'Certificate_Check':
+      command   => "${crt_clean_cmd} ; ${crt_gen_cmd} && ${crt_sign_cmd} && ${cert_find_cmd}",
+      unless    => "/bin/ls ${puppet_ssldir}/certs/${certname}.pem",
+      path      => '/usr/bin:/usr/local/bin',
+      logoutput => on_failure,
+      require   => File[$puppet_conf]
+    }
   }
 
   file { $puppet_docroot:
@@ -104,11 +114,11 @@ class puppet::passenger(
     serveradmin        => $apache_serveradmin,
     servername         => $certname,
     ssl                => true,
-    ssl_cert           => "${puppet_ssldir}/certs/${certname}.pem",
-    ssl_key            => "${puppet_ssldir}/private_keys/${certname}.pem",
-    ssl_chain          => "${puppet_ssldir}/ca/ca_crt.pem",
-    ssl_ca             => "${puppet_ssldir}/ca/ca_crt.pem",
-    ssl_crl            => "${puppet_ssldir}/ca/ca_crl.pem",
+    ssl_cert           => $ssl_cert,
+    ssl_key            => $ssl_key,
+    ssl_chain          => $ssl_chain,
+    ssl_ca             => $ssl_ca,
+    ssl_crl            => $ssl_crl,
     rack_base_uris     => '/',
     custom_fragment    => template('puppet/apache_custom_fragment.erb'),
     require            => [ File['/etc/puppet/rack/config.ru'], File[$puppet_conf] ],
